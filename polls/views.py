5066e98172c102786ab0b3c2b6e524fe0410d34f
Jaso Salgado <jaso_98@hotmail.com>
2020-05-15 16:41:48 -0500

    First commit. I created models, templates, urls and views

---
+++ polls/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+def index(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/index.html', {'question': question})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question})
+
+
+def results(request, question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(question, question_id):
+    return HttpResponse('You are voting on question %s.' % question_id)
+
